a484eb6089ddec3dee633d45c2d567a9aad883b6
Poolitzer <25934244+Poolitzer@users.noreply.github.com>
2019-10-13 22:54:31 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,37 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import * as github from '@actions/github';
 
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+export async function run() {
+    try {
+    const welcomeMessage: string = core.getInput('welcome-message');
+    const welcomeMessage: string = core.getInput('welcome-message', {required: true});
+    const repoToken: string = core.getInput('repo-token', {required: true});
+    const issue: {owner: string; repo: string; number: number} = github.context.issue;
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    if (github.context.payload.action !== 'opened') {
+      console.log('No issue or pull request was opened, skipping');
+      return;
+    }
+    if (github.context.payload.repository) {
+      console.log('Actual a PR');
+    }
+    else {
+      console.log('No pull request was opened, skipping');
+      return;
+    }
+    const client: github.GitHub = new github.GitHub(repoToken);
+    await client.pulls.createReview({
+      owner: issue.owner,
+      repo: issue.repo,
+      pull_number: issue.number,
+      body: welcomeMessage,
+      event: 'COMMENT'
+    });
+    }
+    catch (error) {
+      core.setFailed(error.message);
+      throw error;
+    }
 }
 
 run();
